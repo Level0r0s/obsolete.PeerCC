d9d20d8b1a3ff5850e19918c72417f277796328c
Adam Bartha <abartha@macadamian.com>
2015-07-01 01:24:57 -0700

    Fix crash of PeerCC when IP field in empty for W10(Added validation to UI elements) + fix some build issues

---
+++ PeerConnectionClient.Win10/PeerConnectionClient.Win10/NullToVisibleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace PeerConnectionClient.Utilities
+{
+    class NullToVisibleConverter : IValueConverter
+    {
+        public bool Negated { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
