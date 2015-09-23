3bc715cea905f35bf638608c8981d6a9f867b800
Adam Bartha <abartha@macadamian.com>
2015-09-23 11:45:02 +0300

    Fix video for video only call (when call was initiated with mic off, remote video was green)

--- PeerConnectionClient.Shared/Utilities/SdpUtils.cs
+++ PeerConnectionClient.Shared/Utilities/SdpUtils.cs
@@ -19,54 +19,64 @@ namespace PeerConnectionClient.Utilities
         /// <returns>True if succeeds to force to use the selected audio/video codecs.</returns>
         public static bool SelectCodecs(ref string sdp, CodecInfo audioCodec, CodecInfo videoCodec)
         {
-            if (audioCodec == null)
-            {
-                return false;
-            }
-
-            if (videoCodec == null)
-            {
-              return false;
-            }
-
             Regex mfdRegex = new Regex("\r\nm=audio.*RTP.*?( .\\d*)+\r\n");
             Match mfdMatch = mfdRegex.Match(sdp);
             List<string> mfdListToErase = new List<string>(); //mdf = media format descriptor
-            for (int groupCtr = 1/*Group 0 is whole match*/; groupCtr < mfdMatch.Groups.Count; groupCtr++)
+            bool audioMediaDescFound = mfdMatch.Groups.Count > 1; //Group 0 is whole match
+            if (audioMediaDescFound)
             {
-                for (int captureCtr = 0; captureCtr < mfdMatch.Groups[groupCtr].Captures.Count; captureCtr++)
+                if (audioCodec == null)
                 {
-                    mfdListToErase.Add(mfdMatch.Groups[groupCtr].Captures[captureCtr].Value.TrimStart());
+                    return false;
+                }
+                for (int groupCtr = 1/*Group 0 is whole match*/; groupCtr < mfdMatch.Groups.Count; groupCtr++)
+                {
+                    for (int captureCtr = 0; captureCtr < mfdMatch.Groups[groupCtr].Captures.Count; captureCtr++)
+                    {
+                        mfdListToErase.Add(mfdMatch.Groups[groupCtr].Captures[captureCtr].Value.TrimStart());
+                    }
+                }
+                if (!mfdListToErase.Remove(audioCodec.Id.ToString()))
+                {
+                    return false;
                 }
             }
 
             mfdRegex = new Regex("\r\nm=video.*RTP.*?( .\\d*)+\r\n");
             mfdMatch = mfdRegex.Match(sdp);
-            for (int groupCtr = 1/*Group 0 is whole match*/; groupCtr < mfdMatch.Groups.Count; groupCtr++)
+            bool videoMediaDescFound = mfdMatch.Groups.Count > 1; //Group 0 is whole match
+            if (videoMediaDescFound)
             {
-                for (int captureCtr = 0; captureCtr < mfdMatch.Groups[groupCtr].Captures.Count; captureCtr++)
+                if (videoCodec == null)
                 {
-                    mfdListToErase.Add(mfdMatch.Groups[groupCtr].Captures[captureCtr].Value.TrimStart());
+                    return false;
+                }
+                for (int groupCtr = 1/*Group 0 is whole match*/; groupCtr < mfdMatch.Groups.Count; groupCtr++)
+                {
+                    for (int captureCtr = 0; captureCtr < mfdMatch.Groups[groupCtr].Captures.Count; captureCtr++)
+                    {
+                        mfdListToErase.Add(mfdMatch.Groups[groupCtr].Captures[captureCtr].Value.TrimStart());
+                    }
+                }
+                if (!mfdListToErase.Remove(videoCodec.Id.ToString()))
+                {
+                    return false;
                 }
             }
 
-            if (!mfdListToErase.Remove(audioCodec.Id.ToString()))
+            if (audioMediaDescFound)
             {
-                return false;
+                // Alter audio entry
+                Regex audioRegex = new Regex("\r\n(m=audio.*RTP.*?)( .\\d*)+");
+                sdp = audioRegex.Replace(sdp, "\r\n$1 " + audioCodec.Id);
             }
 
-            if (!mfdListToErase.Remove(videoCodec.Id.ToString()))
+            if (videoMediaDescFound)
             {
-                return false;
+                // Alter video entry
+                Regex videoRegex = new Regex("\r\n(m=video.*RTP.*?)( .\\d*)+");
+                sdp = videoRegex.Replace(sdp, "\r\n$1 " + videoCodec.Id);
             }
-
-            // Alter audio entry
-            Regex audioRegex = new Regex("\r\n(m=audio.*RTP.*?)( .\\d*)+");
-            sdp = audioRegex.Replace(sdp, "\r\n$1 " + audioCodec.Id);
-
-            // Alter video entry
-            Regex videoRegex = new Regex("\r\n(m=video.*RTP.*?)( .\\d*)+");
-            sdp = videoRegex.Replace(sdp, "\r\n$1 " + videoCodec.Id);
 
             // Remove associated rtp mapping, format parameters, feedback parameters
             Regex removeOtherMdfs = new Regex("a=(rtpmap|fmtp|rtcp-fb):(" + String.Join("|", mfdListToErase) + ") .*\r\n");
