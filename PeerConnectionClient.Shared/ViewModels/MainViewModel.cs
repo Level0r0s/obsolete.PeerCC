fbdf5150ee50188a2627547cc9bc2930bd9a1b43
ling zhong <ling@macadamian.com>
2015-10-01 15:14:11 -0400

    fix PCC crash when there is  no network connection during starts.

--- PeerConnectionClient.Shared/ViewModels/MainViewModel.cs
+++ PeerConnectionClient.Shared/ViewModels/MainViewModel.cs
@@ -1723,25 +1723,32 @@ namespace PeerConnectionClient.ViewModels
         /// </summary>
         public static async Task GetNetworkTime()
         {
-          //default Windows time server
-          const string ntpServer = "time.windows.com";
+            //default Windows time server
+            const string ntpServer = "time.windows.com";
 
-          // NTP message size - 16 bytes of the digest (RFC 2030)
-          byte[] ntpData = new byte[48];
+            // NTP message size - 16 bytes of the digest (RFC 2030)
+            byte[] ntpData = new byte[48];
 
-          //Setting the Leap Indicator, Version Number and Mode values
-          ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+            //Setting the Leap Indicator, Version Number and Mode values
+            ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
 
 
-          //NTP uses UDP
-          var socket = new Windows.Networking.Sockets.DatagramSocket();
-          socket.MessageReceived += OnNTPTimeReceived;
+            //NTP uses UDP
+            var socket = new Windows.Networking.Sockets.DatagramSocket();
+            socket.MessageReceived += OnNTPTimeReceived;
 
-          //The UDP port number assigned to NTP is 123
-          await socket.ConnectAsync(new Windows.Networking.HostName(ntpServer), "123");
+            try
+            {
+                //The UDP port number assigned to NTP is 123
+                await socket.ConnectAsync(new Windows.Networking.HostName(ntpServer), "123");
+                await socket.OutputStream.WriteAsync(ntpData.AsBuffer());
 
-          await socket.OutputStream.WriteAsync(ntpData.AsBuffer());
-
+            }
+            catch (Exception e)
+            {
+                MessageDialog dialog = new MessageDialog("Failed To sync with ntp server.");
+                dialog.ShowAsync();
+            }
 
         }
 
@@ -1752,29 +1759,29 @@ namespace PeerConnectionClient.ViewModels
         /// <param name="eventArguments">event information</param>
         static async void OnNTPTimeReceived(Windows.Networking.Sockets.DatagramSocket socket, Windows.Networking.Sockets.DatagramSocketMessageReceivedEventArgs eventArguments)
         {
-          byte[] ntpData = new byte[48];
-   
-          eventArguments.GetDataReader().ReadBytes(ntpData);
+            byte[] ntpData = new byte[48];
 
-          //Offset to get to the "Transmit Timestamp" field (time at which the reply 
-          //departed the server for the client, in 64-bit timestamp format."
-          const byte serverReplyTime = 40;
+            eventArguments.GetDataReader().ReadBytes(ntpData);
 
-          //Get the seconds part
-          ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
+            //Offset to get to the "Transmit Timestamp" field (time at which the reply 
+            //departed the server for the client, in 64-bit timestamp format."
+            const byte serverReplyTime = 40;
 
-          //Get the seconds fraction
-          ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
+            //Get the seconds part
+            ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
 
-          //Convert From big-endian to little-endian
-          intPart = SwapEndianness(intPart);
-          fractPart = SwapEndianness(fractPart);
+            //Get the seconds fraction
+            ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
 
-          ulong milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
+            //Convert From big-endian to little-endian
+            intPart = SwapEndianness(intPart);
+            fractPart = SwapEndianness(fractPart);
 
-          WebRTC.SynNTPTime((long)milliseconds);
+            ulong milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
 
-          socket.Dispose();
+            WebRTC.SynNTPTime((long)milliseconds);
+
+            socket.Dispose();
 
 
         }
@@ -1782,10 +1789,10 @@ namespace PeerConnectionClient.ViewModels
 
         static uint SwapEndianness(ulong x)
         {
-          return (uint)(((x & 0x000000ff) << 24) +
-                         ((x & 0x0000ff00) << 8) +
-                         ((x & 0x00ff0000) >> 8) +
-                         ((x & 0xff000000) >> 24));
+            return (uint)(((x & 0x000000ff) << 24) +
+                           ((x & 0x0000ff00) << 8) +
+                           ((x & 0x00ff0000) >> 8) +
+                           ((x & 0xff000000) >> 24));
         }
     }
 }
