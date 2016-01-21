739f24de1872f0dcb838496960badabd243d6623
Bakhshi Mnatsakanyan <bmnatsakanyan@macadamian.com>
2016-01-21 15:04:17 +0400

    [Bug] VS Add-in: memory usage graph show up empty
    [Description]
    - The memory usage values doesn't show up on the plugin, the graph is always empty!
    
    [Changes]
    
    - modify PROCESS_MEMORY_COUNTERS_EX structure to have correct
      memory layout(size) for x86 and x64 compiled apps. Previously the
      GetProcessMemoryInfo failed with ERROR_INSUFFICIENT_BUFFER error for x64
      compiled app.
    
    [Test Cases]
    1. Build PeerCC(both x86 and x64)
    2. Establish PeerCC <-> PeerCC call
    3. Start VS add-in and collect statistics
    https://microsoft.sharepoint.com/teams/Real_Time_Communication_for_WinRT/_layouts/15/WopiFrame.aspx?sourcedoc={d0164c70-cb5a-4002-a3d3-91ac6f483d9b}&action=edit&wd=target%28%2F%2FEverything%20you%20need%20to%20start.one%7C60afa497-bbdb-46f3-84fd-d81e11a8b4fb%2FVisual%20Studio%20Add-In%7C3443625f-0811-4a2c-abaf-5ab3a16acfa5%2F%29
    
    Expected Result: The MemUsage graph shows correct values

--- PeerConnectionClient.Shared/Utilities/AppPerf.cs
+++ PeerConnectionClient.Shared/Utilities/AppPerf.cs
@@ -37,10 +37,9 @@ namespace PeerConnectionClient.Utilities
 
             if (GetProcessMemoryInfo(GetCurrentProcess(), out memoryCounters, memoryCounters.cb))
             {
-                ret = memoryCounters.PrivateUsage;
+                ret = memoryCounters.PrivateUsage.ToInt64();
 
             }
-
             Debug.WriteLine(ret);
 
             return ret;
@@ -767,15 +766,15 @@ namespace PeerConnectionClient.Utilities
      {
          public uint cb;             // The size of the structure, in bytes (DWORD).
          public uint PageFaultCount;         // The number of page faults (DWORD).
-         public uint PeakWorkingSetSize;     // The peak working set size, in bytes (SIZE_T).
-         public uint WorkingSetSize;         // The current working set size, in bytes (SIZE_T).
-         public uint QuotaPeakPagedPoolUsage;    // The peak paged pool usage, in bytes (SIZE_T).
-         public uint QuotaPagedPoolUsage;    // The current paged pool usage, in bytes (SIZE_T).
-         public uint QuotaPeakNonPagedPoolUsage; // The peak nonpaged pool usage, in bytes (SIZE_T).
-         public uint QuotaNonPagedPoolUsage;     // The current nonpaged pool usage, in bytes (SIZE_T).
-         public uint PagefileUsage;          // The Commit Charge value in bytes for this process (SIZE_T). Commit Charge is the total amount of memory that the memory manager has committed for a running process.
-         public uint PeakPagefileUsage;      // The peak value in bytes of the Commit Charge during the lifetime of this process (SIZE_T).
-         public uint PrivateUsage;           
+         public IntPtr PeakWorkingSetSize;     // The peak working set size, in bytes (SIZE_T).
+         public IntPtr WorkingSetSize;         // The current working set size, in bytes (SIZE_T).
+         public IntPtr QuotaPeakPagedPoolUsage;    // The peak paged pool usage, in bytes (SIZE_T).
+         public IntPtr QuotaPagedPoolUsage;    // The current paged pool usage, in bytes (SIZE_T).
+         public IntPtr QuotaPeakNonPagedPoolUsage; // The peak nonpaged pool usage, in bytes (SIZE_T).
+         public IntPtr QuotaNonPagedPoolUsage;     // The current nonpaged pool usage, in bytes (SIZE_T).
+         public IntPtr PagefileUsage;          // The Commit Charge value in bytes for this process (SIZE_T). Commit Charge is the total amount of memory that the memory manager has committed for a running process.
+         public IntPtr PeakPagefileUsage;      // The peak value in bytes of the Commit Charge during the lifetime of this process (SIZE_T).
+         public IntPtr PrivateUsage;
      }
 
 }
