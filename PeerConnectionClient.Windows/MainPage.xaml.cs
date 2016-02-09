20580f42d82b5923b10e4d2f9d718bf4cc8215f6
unknown <sgrigoryan@macadamian.com>
2016-02-09 22:36:27 +0400

    Added copyright comment to all cs files

--- PeerConnectionClient.Windows/MainPage.xaml.cs
+++ PeerConnectionClient.Windows/MainPage.xaml.cs
@@ -1,4 +1,15 @@
-﻿using Windows.UI.ApplicationSettings;
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using Windows.UI.ApplicationSettings;
 using Windows.UI.Xaml.Navigation;
 using PeerConnectionClient.SettingsFlyouts;
 using PeerConnectionClient.ViewModels;
--- PeerConnectionClient.WindowsPhone/MainPage.xaml.cs
+++ PeerConnectionClient.WindowsPhone/MainPage.xaml.cs
@@ -1,4 +1,15 @@
-﻿using PeerConnectionClient.ViewModels;
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using PeerConnectionClient.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
